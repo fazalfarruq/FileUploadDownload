d252f3b5f6bf3f0ebd27e8b0332ac4616e3be120
Fazal Farruq <fazalfarruq@gmail.com>
2024-01-03 21:01:34 -0500

    Add project files.

---
+++ FileUploadDownload/Controllers/FilesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadDownload.Controllers
+{
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpPost]
+        [Route("UploadFile")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UploadFile(IFormFile file, CancellationToken cancellationToken)
+        {
+           var result = await WriteFile(file);
+            return Ok(result);
+        }
+
+        private async Task<string> WriteFile(IFormFile file)
+        {
+            // Get file name
+            string fileName = "";
+
+            try
+            {
+                var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+                fileName = DateTime.Now.Ticks + extension; //Create a new Name for the file due to security reasons.
+
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\Files");
+
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+
+                var exactpath = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\Files", fileName);
+
+                await using var stream = new FileStream(exactpath, FileMode.Create);
+                await file.CopyToAsync(stream);
+            }
+            catch (Exception e)
+            {
+                //TODO: handle exception
+            }
+
+            return fileName;
+        }
+
+    }
+}
